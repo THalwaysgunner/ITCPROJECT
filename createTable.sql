3310991a424c4e72a64b1e6c0917276bbae7b71a
haim attias <haimattias@haims-MacBook-Pro.local>
2021-04-03 18:31:40 +0300

    fix createtable.sql bugs and functionality

--- createTable.sql
+++ createTable.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE stocks;
-
+drop database  stocks;
 use stocks;
 
 DROP TABLE stock_general_info;
@@ -11,54 +11,38 @@ CREATE TABLE `stock_general_info`(
   `volume` varchar(25),
   `market_cap` varchar(25),
   `description` text,
-  `status` boolean default 0
+  `status` boolean
 );
 
 DROP TABLE stock_executive;
 CREATE TABLE `stock_executive`(
-  `stock_id` varchar(20),
+  `stock_id` varchar(20) references stock_general_info(ID),
   `name_of_ex` text(25),
   `title` text,
-  `salary` varchar(25),
-  foreign key (stock_id) references stock_general_info(ID)
+  `salary` varchar(25)
 );
 
 DROP TABLE financial_info;
 CREATE TABLE `financial_info` (
-  `stock_id` varchar(20),
+  `stock_id` varchar(20) references stock_general_info(ID),
   `TTM_revenue` varchar(15),
   `TTM_gross_profit` varchar(15),
   `TTM_expense` varchar(15),
-  `TTM_cost_of_revenue` varchar(15),
-  foreign key (stock_id) references stock_general_info(ID)
+  `TTM_cost_of_revenue` varchar(15)
 );
 
 DROP TABLE news;
 CREATE TABLE `news` (
-  `stock_id` varchar(20),
+  `stock_id` varchar(20) references stock_general_info(ID),
   `title` text,
-  `news_link` text,
-  foreign key (stock_id) references stock_general_info(ID)
+  `news_link` text
 );
 
 DROP TABLE historical_prices;
 CREATE TABLE `historical_prices`(
-    `stock_id` varchar(20),
+    `stock_id` varchar(20) references stock_general_info(ID),
     `Date` datetime ,
-    `price` varchar(15),
-    foreign key (stock_id) references stock_general_info(ID)
+    `price` varchar(15)
 );
 
 
-select * from historical_prices;
-
-
-
-
-ALTER TABLE `stock_general_info` ADD FOREIGN KEY (`ID`) REFERENCES `stock_executive` (`stock_id`);
-
-ALTER TABLE `stock_general_info` ADD FOREIGN KEY (`ID`) REFERENCES `financial_info` (`stock_id`);
-
-ALTER TABLE `stock_general_info` ADD FOREIGN KEY (`ID`) REFERENCES `news` (`stock_id`);
-
-ALTER TABLE `stock_general_info` ADD FOREIGN KEY (`ID`) REFERENCES `historical_prices` (`stock_id`);
